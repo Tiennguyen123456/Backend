70001d77c70ddd7458a95522815f59ee656ea594
SyDev <sydev.work@gmail.com>
2024-03-22 21:50:41 +0700

    Form landing page

--- src/app/[locale]/landingpage/page.tsx
+++ src/app/[locale]/landingpage/page.tsx
@@ -1,21 +1,48 @@
 ﻿"use client";
+import { z } from "zod";
 import { Button } from "@/components/ui/button";
-import {
-    Dialog,
-    DialogClose,
-    DialogContent,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-    DialogDescription,
-} from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import SpanRequired from "@/components/ui/span-required";
+import { phoneRegExp } from "@/constants/variables";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useTranslations } from "next-intl";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { toastSuccess } from "@/utils/toast";
 
 export default function LandingPage() {
+    // ** I18n
+    const translation = useTranslations("");
+
+    // ** State
     const [open, setOpen] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const formSchema = z.object({
+        name: z.string().min(1, { message: translation("error.requiredName") }),
+        email: z.string().email({ message: translation("error.invalidEmail") }),
+        phone: z.string().regex(phoneRegExp, { message: translation("error.invalidPhone") }),
+        address: z.string().min(1, { message: translation("error.requiredAddress") }),
+    });
+    type FormValues = z.infer<typeof formSchema>;
+    const form = useForm<FormValues>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+            phone: "",
+            address: "",
+        },
+    });
+    let onSubmit = async (data: FormValues) => {
+        setLoading(true);
+        toastSuccess("Gửi thông tin thàng công!");
+        setTimeout(() => {
+            window.location.reload();
+        }, 2000);
+    };
     return (
         <div
             style={{
@@ -40,42 +67,111 @@ export default function LandingPage() {
                 >
                     <DialogContent className="sm:max-w-[425px]">
                         <DialogHeader>
-                            <DialogTitle className="text-center">Form đăng ký</DialogTitle>
-                            <DialogDescription>
-                                Make changes to your profile here. Click save when you're done.
-                            </DialogDescription>
+                            <DialogTitle className="text-2xl text-center">Form Thông Tin</DialogTitle>
                         </DialogHeader>
-                        <div className="grid gap-4 py-4">
-                            <div className="grid grid-cols-4 items-center gap-4">
-                                <Label
-                                    htmlFor="name"
-                                    className="text-right"
-                                >
-                                    Name
-                                </Label>
-                                <Input
-                                    id="name"
-                                    defaultValue="Pedro Duarte"
-                                    className="col-span-3"
+                        <Form {...form}>
+                            <form
+                                onSubmit={form.handleSubmit(onSubmit)}
+                                className="grid gap-4"
+                            >
+                                <FormField
+                                    control={form.control}
+                                    name="name"
+                                    render={({ field }) => (
+                                        <FormItem className="grid grid-cols-4 items-center gap-2">
+                                            <FormLabel className="text-right">
+                                                {translation("label.name")}
+                                                <SpanRequired />
+                                            </FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    className="h-10 col-span-3"
+                                                    disabled={loading}
+                                                    placeholder={translation("placeholder.name")}
+                                                    {...field}
+                                                />
+                                            </FormControl>
+                                            <div className="col-span-1"></div>
+                                            <FormMessage className="col-span-3" />
+                                        </FormItem>
+                                    )}
                                 />
-                            </div>
-                            <div className="grid grid-cols-4 items-center gap-4">
-                                <Label
-                                    htmlFor="username"
-                                    className="text-right"
-                                >
-                                    Username
-                                </Label>
-                                <Input
-                                    id="username"
-                                    defaultValue="@peduarte"
-                                    className="col-span-3"
+                                <FormField
+                                    control={form.control}
+                                    name="email"
+                                    render={({ field }) => (
+                                        <FormItem className="grid grid-cols-4 items-center gap-2">
+                                            <FormLabel className="text-right">
+                                                {translation("label.email")}
+                                                <SpanRequired />
+                                            </FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    className="h-10 col-span-3"
+                                                    disabled={loading}
+                                                    placeholder={translation("placeholder.email")}
+                                                    {...field}
+                                                />
+                                            </FormControl>
+                                            <div className="col-span-1"></div>
+                                            <FormMessage className="col-span-3" />
+                                        </FormItem>
+                                    )}
                                 />
-                            </div>
-                        </div>
-                        <DialogFooter>
-                            <Button type="submit">Save changes</Button>
-                        </DialogFooter>
+                                <FormField
+                                    control={form.control}
+                                    name="phone"
+                                    render={({ field }) => (
+                                        <FormItem className="grid grid-cols-4 items-center gap-2">
+                                            <FormLabel className="text-right">
+                                                {translation("label.phone")}
+                                                <SpanRequired />
+                                            </FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    className="h-10 col-span-3"
+                                                    disabled={loading}
+                                                    placeholder={translation("placeholder.phone")}
+                                                    {...field}
+                                                />
+                                            </FormControl>
+                                            <div className="col-span-1"></div>
+                                            <FormMessage className="col-span-3" />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="address"
+                                    render={({ field }) => (
+                                        <FormItem className="grid grid-cols-4 items-center gap-2">
+                                            <FormLabel className="text-right">
+                                                {translation("label.address")}
+                                                <SpanRequired />
+                                            </FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    className="h-10 col-span-3"
+                                                    disabled={loading}
+                                                    placeholder={translation("placeholder.address")}
+                                                    {...field}
+                                                />
+                                            </FormControl>
+                                            <div className="col-span-1"></div>
+                                            <FormMessage className="col-span-3" />
+                                        </FormItem>
+                                    )}
+                                />
+                                <Button
+                                    disabled={loading}
+                                    variant="default"
+                                    type={"submit"}
+                                    className="mt-3"
+                                >
+                                    {translation("action.submit")}
+                                </Button>
+                            </form>
+                        </Form>
                     </DialogContent>
                 </Dialog>
             </div>
