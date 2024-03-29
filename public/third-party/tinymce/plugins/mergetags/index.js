020f17c8f95d832d0a976e699fdfa199c7c08406
Tan Le Minh <minhtan1581@gmail.com>
2024-03-29 22:05:41 +0700

    Fix tinymce self-hosted

---
+++ public/third-party/tinymce/plugins/mergetags/index.js
@@ -0,0 +1,7 @@
+// Exports the "mergetags" plugin for usage with module loaders
+// Usage:
+//   CommonJS:
+//     require('tinymce/plugins/mergetags')
+//   ES2015:
+//     import 'tinymce/plugins/mergetags'
+require('./plugin.js');
